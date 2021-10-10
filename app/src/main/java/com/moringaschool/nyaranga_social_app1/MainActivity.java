e7342740ca0cd60fdd1bf3c27b0ecdd6930a337d
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-10 22:23:10 +0300

    Declared button and textView

--- app/src/main/java/com/moringaschool/nyaranga_social_app1/MainActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app1/MainActivity.java
@@ -3,9 +3,12 @@ package com.moringaschool.nyaranga_social_app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView registerText;
+    Button newActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
