35432fc3248297a72cb502c13caacdb2a7532688
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-11 00:41:27 +0300

    Added butterknife to Home Activity

--- app/src/main/java/com/moringaschool/nyaranga_social_app1/HomeActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app1/HomeActivity.java
@@ -8,14 +8,19 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+    @BindView(R.id.gridy) GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        GridView gridView = findViewById(R.id.gridy);
+        ButterKnife.bind(this);
+//        GridView gridView = findViewById(R.id.gridy);
         gridView.setAdapter(new Adaptery(this));
         gridView.setNumColumns(2);
 
