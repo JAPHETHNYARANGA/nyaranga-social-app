f6647204ada199b51da1bf995b580cea30171be8
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-10 22:26:55 +0300

    created onclick listener to text view

--- app/src/main/java/com/moringaschool/nyaranga_social_app1/MainActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app1/MainActivity.java
@@ -2,7 +2,9 @@ package com.moringaschool.nyaranga_social_app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
         registerText = (TextView)findViewById(R.id.tvRegister);
         newActivity = (Button)findViewById(R.id.button1);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
