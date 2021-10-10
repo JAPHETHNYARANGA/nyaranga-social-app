2acafef56eaad06d6a1cd6c13971aec0a3dc228d
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-11 00:20:42 +0300

    Declared the login button

--- app/src/main/java/com/moringaschool/nyaranga_social_app1/Register.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app1/Register.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     TextView logintText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
