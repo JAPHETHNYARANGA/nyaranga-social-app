4204be8ac8d3ea49e1347875aeda9cae8c99c8e1
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-10 22:32:59 +0300

    made the textview navigate to a new Activity

--- app/src/main/java/com/moringaschool/nyaranga_social_app1/Register.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app1/Register.java
@@ -2,7 +2,9 @@ package com.moringaschool.nyaranga_social_app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
@@ -12,5 +14,15 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        logintText=(TextView)findViewById(R.id.textView2);
+
+
+        logintText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
